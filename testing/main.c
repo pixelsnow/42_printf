e5a9fc9ae836b00f0e95f87272d44aa510105853
Valeria Vagapova <lera.vagapova@gmail.com>
2022-12-23 21:08:05 +0200

    add more test cases

--- testing/main.c
+++ testing/main.c
@@ -6,11 +6,11 @@
 /*   By: vvagapov <vvagapov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/23 16:53:23 by vvagapov          #+#    #+#             */
-/*   Updated: 2022/12/23 18:32:00 by vvagapov         ###   ########.fr       */
+/*   Updated: 2022/12/23 21:02:54 by vvagapov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../printf.h"
+#include "../ft_printf.h"
 #include <limits.h>
 #include <stdio.h>
 
@@ -33,6 +33,10 @@ void	test_c(void)
 	test_char('\0');
 	test_char(100);
 	test_char(55);
+	test_char('0' - 256);
+	test_char('0' + 256);
+	test_char(' ');
+	test_char(0);
 }
 
 void	test_string(char *s)
@@ -63,6 +67,7 @@ void	test_s(void)
 	test_string("");
 	test_string(str);
 	test_string("hepjaerrouh08q33 q3 977y3 0qivy;  jdjsrj	f");
+	test_string("-");
 }
 
 void	test_pointer(void *p)
@@ -89,6 +94,17 @@ void	test_p(void)
 	test_pointer(&a);
 	test_pointer(s);
 	test_pointer(NULL);
+	test_pointer(-1);
+	test_pointer(1);
+	test_pointer(15);
+	test_pointer(16);
+	test_pointer(17);
+	test_pointer(LONG_MIN);
+	test_pointer(LONG_MAX);
+	test_pointer(INT_MIN);
+	test_pointer(INT_MAX);
+	test_pointer(ULONG_MAX);
+	test_pointer(-ULONG_MAX);
 }
 
 void	test_decimal(int n)
@@ -115,6 +131,11 @@ void	test_d(void)
 	test_decimal(0);
 	test_decimal(INT_MIN);
 	test_decimal(INT_MAX);
+	test_decimal(LONG_MAX);
+	test_decimal(LONG_MIN);
+	test_decimal(UINT_MAX);
+	test_decimal(ULONG_MAX);
+	test_decimal(9223372036854775807LL);
 }
 
 void	test_integer(int n)
@@ -141,6 +162,11 @@ void	test_i(void)
 	test_integer(0);
 	test_integer(INT_MIN);
 	test_integer(INT_MAX);
+	test_decimal(LONG_MAX);
+	test_decimal(LONG_MIN);
+	test_decimal(UINT_MAX);
+	test_decimal(ULONG_MAX);
+	test_decimal(9223372036854775807LL);
 }
 
 void	test_unsigned(unsigned int n)
@@ -165,6 +191,21 @@ void	test_u(void)
 	test_unsigned(0);
 	test_unsigned(UINT_MAX);
 	test_unsigned(-1);
+	test_unsigned(99);
+	test_unsigned(100);
+	test_unsigned(101);
+	test_unsigned(-9);
+	test_unsigned(-99);
+	test_unsigned(-100);
+	test_unsigned(INT_MAX);
+	test_unsigned(INT_MIN);
+	test_unsigned(LONG_MAX);
+	test_unsigned(LONG_MIN);
+	test_unsigned(UINT_MAX);
+	test_unsigned(ULONG_MAX);
+	test_unsigned(9223372036854775807LL);
+	test_unsigned(-42);
+	
 }
 
 void	test_hex_lowercase(unsigned int n)
@@ -190,6 +231,21 @@ void	test_x(void)
 	test_hex_lowercase(0);
 	test_hex_lowercase(UINT_MAX);
 	test_hex_lowercase(-1);
+	test_hex_lowercase(15);
+	test_hex_lowercase(16);
+	test_hex_lowercase(17);
+	test_hex_lowercase(-15);
+	test_hex_lowercase(-16);
+	test_hex_lowercase(-17);
+	test_hex_lowercase(-100);
+	test_hex_lowercase(INT_MAX);
+	test_hex_lowercase(INT_MIN);
+	test_hex_lowercase(LONG_MAX);
+	test_hex_lowercase(LONG_MIN);
+	test_hex_lowercase(UINT_MAX);
+	test_hex_lowercase(ULONG_MAX);
+	test_hex_lowercase(9223372036854775807LL);
+	test_hex_lowercase(-42);
 }
 
 void	test_hex_uppercase(unsigned int n)
@@ -215,6 +271,21 @@ void	test_xx(void)
 	test_hex_uppercase(0);
 	test_hex_uppercase(UINT_MAX);
 	test_hex_uppercase(-1);
+	test_hex_uppercase(15);
+	test_hex_uppercase(16);
+	test_hex_uppercase(17);
+	test_hex_uppercase(-15);
+	test_hex_uppercase(-16);
+	test_hex_uppercase(-17);
+	test_hex_uppercase(-100);
+	test_hex_uppercase(INT_MAX);
+	test_hex_uppercase(INT_MIN);
+	test_hex_uppercase(LONG_MAX);
+	test_hex_uppercase(LONG_MIN);
+	test_hex_uppercase(UINT_MAX);
+	test_hex_uppercase(ULONG_MAX);
+	test_hex_uppercase(9223372036854775807LL);
+	test_hex_uppercase(-42);
 }
 
 void	test_percent_sign(void)
@@ -222,9 +293,9 @@ void	test_percent_sign(void)
 	int	len_orig;
 	int	len_mine;
 
-	len_orig = printf("ORIG: '%%'");
+	len_orig = printf("ORIG: '%%    %%%%'");
 	printf("\n");
-	len_mine = ft_printf("MINE: '%%'");
+	len_mine = ft_printf("MINE: '%%    %%%%'");
 	printf("\n");
 	if (len_orig != len_mine)
 		printf("----- LENGTH DOESN'T MATCH! %d != %d -----\n", len_orig,
@@ -237,16 +308,13 @@ void	test_percent(void)
 	test_percent_sign();
 }
 
-void	test_extra(char *s)
+void	test_extra(const char *s)
 {
 	int	len_orig;
 	int	len_mine;
 
-	printf("ORIG:");
 	len_orig = printf(s);
-	ft_printf("\nMINE:");
 	len_mine = ft_printf(s);
-	ft_printf("\n");
 	if (len_orig != len_mine)
 		printf("----- LENGTH DOESN'T MATCH! %d != %d -----\n", len_orig,
 				len_mine);
@@ -255,9 +323,9 @@ void	test_extra(char *s)
 void	test_extras(void)
 {
 	printf("\n--- EXTRA TEST ---\n\n");
-	test_extra("%\n");
-	test_extra("%");
-	test_extra("df%yjh");
+	test_extra("\n1%\n");
+	test_extra("\n2%");
+	test_extra("\n3df%yjh\n");
 }
 
 int	main(void)
@@ -278,4 +346,5 @@ int	main(void)
 /*
 ft_printf("%\n");
 null pointer
+ft_printf("%.c", 'a')
 */
